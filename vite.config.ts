dd116bd173b208219b1ae23a94e4c7cc83a82b5c
vboechat <victor2005contato@gmail.com>
2023-04-09 14:07:09 -0300

    build: update package to support vite and vite-plugin-pwa

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "VBoechat Link-tree",
+        short_name: "VBoechat Link-tree",
+        description: "VBoechat personal link-tree",
+        display: "standalone",
+        start_url: "/",
+        theme_color: "#1488CC",
+        background_color: "#1488CC",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/maskable_icon.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+});
